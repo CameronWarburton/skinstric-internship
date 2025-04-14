74b4752d25a7faf5d20df2a0b18f14317bacafcd
Cameron <cameron.cw@outlook.com>
2025-04-14 12:55:44 +0100

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section id="footer">
+        <footer className="footer__container">
+          <p className="footer__content">
+            SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALISED
+            ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS.
+          </p>
+        </footer>
+    </section>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
                     <h1 className='header__subtitle'>skincare</h1>
                 </div>
             </header>
+            <Footer />
         </div>
     </section>
   )
