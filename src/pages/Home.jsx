e9d015b44b039582d23904f5356ff72247f8738d
Cameron <cameron.cw@outlook.com>
2025-04-15 13:16:46 +0100

    Footer Completed. Made a start on buttons either side of the title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import RectangleLeft from "../assets/Rectangle Left.png"
 
 const Home = () => {
   return (
     <section id="landing">
         <div className='row'>
+          <div className="header__container">
+            <figure className='header__img--wrapper'>
+              <img className='header__img' src={RectangleLeft} alt="" />
+            </figure>
             <header className='header'>
                 <div className="header__content">
                     <h1 className='header__title'>Sophisticated</h1>
                     <h1 className='header__subtitle'>skincare</h1>
                 </div>
             </header>
+          </div>
             <Footer />
         </div>
     </section>
