048a586e7ad14b4997f0dd8c166f1cc2baecf8e7
Cameron <cameron.cw@outlook.com>
2025-04-22 13:06:06 +0100

    Landing Page Positioning Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Footer from '../components/Footer'
 import RectangleLeft from "../assets/Rectangle Left.png"
 import RectangleRight from "../assets/Rectangle Right.png"
+import TakeTest from "../assets/button-take-test.png"
+import DiscoverAI from "../assets/button-discover-AI.png"
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
           <div className="header__container">
             <figure className='header__img--wrapper'>
               <img className='header__img' src={RectangleLeft} alt="" />
+              <img className='header__btn--left' src={DiscoverAI} />
             </figure>
             <header className='header'>
                 <div className="header__content">
@@ -18,6 +21,7 @@ const Home = () => {
             </header>
             <figure className='header__img--wrapper'>
               <img className='header__img' src={RectangleRight} alt="" />
+              <img className='header__btn--right' src={TakeTest} />
             </figure>
           </div>
             <Footer />
